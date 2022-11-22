fffb1e20451054661dcb7a5df4d021918d6e4ac9
lican <lican.m.olea@gmail.com>
2022-11-22 14:42:51 -0300

    mi pagian

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class="Nav">
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+</div>
+      <div class="container text-center">
+        <div class="row row-cols-2">
+          <div class="col">
+            <img src="./fotonom.jpeg" class="img-fluid rounded-start" width="250" height="250" />
+          </div>
+          <div class="col">
+            <br/>
+            <h5 class="card-title">Micol Iara Herrera Salerno</h5>
+            <br/>
+            <p className="text-end">Desarrolladora de citio web. En buscqueda de un buen ambiente laboral con grandes desafio y mas que todo con muchas ganas de aprender cosas nuevas , donde la cratividad sea un elemento fundamental para el desarrollo del trabajo tanto individual como en el equipo.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
